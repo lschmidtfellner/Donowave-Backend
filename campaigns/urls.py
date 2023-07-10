b7ca10c0d5c034847e70cf6a0f800a981312a3a9
mikefess <62866256+Drofessional@users.noreply.github.com>
2023-07-10 14:43:22 -0400

    User Create fixed?

--- campaigns/urls.py
+++ campaigns/urls.py
@@ -1,12 +1,13 @@
-# urls.py in your campaigns app
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CampaignViewSet, DonationViewSet
+from .views import CampaignViewSet, DonationViewSet, UserCreate
 
 router = DefaultRouter()
 router.register(r'campaigns', CampaignViewSet)
 router.register(r'donations', DonationViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
+    path('api/register/', UserCreate.as_view(), name='user-register'),
+    path('api-auth/', include('rest_framework.urls')),
 ]
