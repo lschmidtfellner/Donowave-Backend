bb8806d2d50763e0a79d7708931b1c6fa0537b77
kevininga-ga <kevininga813@gmail.com>
2023-07-10 15:09:18 -0400

    added django_heroku to installs

--- fundraiser/settings.py
+++ fundraiser/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'fundraiser.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 import dj_database_url
-# import django_heroku
+import django_heroku
 
 import environ  
 # Initialise environment variables
