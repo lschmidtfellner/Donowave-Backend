ec9a4afbf93f81dcc17051e02edfb8ee34f8673c
kevininga-ga <kevininga813@gmail.com>
2023-07-10 12:55:17 -0400

    changed database in settings

--- fundraiser/settings.py
+++ fundraiser/settings.py
@@ -77,10 +77,14 @@ WSGI_APPLICATION = 'fundraiser.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'campaigns',
+        'USER': 'campaigns_admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost'
     }
 }
+
 
 
 # Password validation
