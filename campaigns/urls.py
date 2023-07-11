2461c3558a089d6675fec21ee40c7c496446d91c
mikefess <62866256+Drofessional@users.noreply.github.com>
2023-07-11 19:20:52 -0400

    Added back routes in backend for user-specific campaigns/donations

--- campaigns/urls.py
+++ campaigns/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CampaignViewSet, DonationViewSet, UserCreate, CustomAuthToken
+from .views import CampaignViewSet, DonationViewSet, UserCreate, CustomAuthToken, UserDetail, UserDonations, UserCampaigns, UserList
 
 router = DefaultRouter()
 router.register(r'campaigns', CampaignViewSet)
@@ -11,4 +11,8 @@ urlpatterns = [
     path('register/', UserCreate.as_view(), name='user-register'),
     path('login/', CustomAuthToken.as_view(), name='user-login'),
     path('api-auth/', include('rest_framework.urls')),
+    path('users/', UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserDetail.as_view(), name='user-detail'),
+    path('users/<int:pk>/donations/', UserDonations.as_view(), name='user-donations'),
+    path('users/<int:pk>/campaigns/', UserCampaigns.as_view(), name='user-campaigns'),
 ]
