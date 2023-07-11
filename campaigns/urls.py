54f329fd72e42cbca6719f3827f12bc85872ce2b
mikefess <62866256+Drofessional@users.noreply.github.com>
2023-07-11 14:22:51 -0400

    Added routes for getting user specific campaigns/donations and all users list and more

--- campaigns/urls.py
+++ campaigns/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CampaignViewSet, DonationViewSet, UserCreate
+from .views import CampaignViewSet, DonationViewSet, UserCreate, UserViewSet
 
 router = DefaultRouter()
 router.register(r'campaigns', CampaignViewSet)
 router.register(r'donations', DonationViewSet)
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- campaigns/views.py
+++ campaigns/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets, generics
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Campaign, Donation
 from .serializers import CampaignSerializer, DonationSerializer, UserSerializer
 
@@ -9,6 +10,24 @@ class UserCreate(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = []  # No permission for registration view
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    @action(detail=True, methods=['get'])
+    def donations(self, request, pk=None):
+        user = self.get_object()
+        donations = Donation.objects.filter(user=user)
+        serializer = DonationSerializer(donations, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['get'])
+    def campaigns(self, request, pk=None):
+        user = self.get_object()
+        campaigns = Campaign.objects.filter(user=user)
+        serializer = CampaignSerializer(campaigns, many=True)
+        return Response(serializer.data)
 
 class CampaignViewSet(viewsets.ModelViewSet):
     queryset = Campaign.objects.all()
@@ -20,6 +39,13 @@ class CampaignViewSet(viewsets.ModelViewSet):
         else:
             self.permission_classes = [AllowAny,]
         return super(CampaignViewSet, self).get_permissions()
+
+    @action(detail=True, methods=['get'])
+    def donations(self, request, pk=None):
+        campaign = self.get_object()
+        donations = Donation.objects.filter(campaign=campaign)
+        serializer = DonationSerializer(donations, many=True)
+        return Response(serializer.data)
 
 class DonationViewSet(viewsets.ModelViewSet):
     queryset = Donation.objects.all()
